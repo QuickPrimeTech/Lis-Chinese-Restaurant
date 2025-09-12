dd6e0ce48279a12f422478ee0b32377c250d663f
Meshack-kipkemoi <meshackkipkemoi3@gmail.com>
2025-09-12 17:32:48 +0300

    I have made the private events section

---
+++ src/app/private-events/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import PrivateEvents from "@/sections/private-events/PrivateEvents";
+const page = () => {
+  return (
+    <div>
+      <PrivateEvents />
+    </div>
+  );
+};
+
+export default page;
